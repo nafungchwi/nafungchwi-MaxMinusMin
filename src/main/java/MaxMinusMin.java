9630ccee9a7a54a8580f1a983bfd2bb55ed522f9
Ngwashi Ronald Afungchwi <ngwashiron@gmail.com>
2023-03-04 18:41:18 +0000

    the difference the max and min values in a list

--- src/main/java/MaxMinusMin.java
+++ src/main/java/MaxMinusMin.java
@@ -1,4 +1,5 @@
 
+import java.util.Collections;
 import java.util.List;
 
 public class MaxMinusMin {
@@ -10,6 +11,11 @@ public class MaxMinusMin {
      * @return the max value of nums minus the min value of nums.
      */
     public int difference(List<Integer> nums){
-        return 0;
+        if(nums == null || nums.isEmpty()){
+            throw new IllegalArgumentException("nums must not be null or empty");
+        }
+        int max = Collections.max(nums);
+        int min = Collections.min(nums);
+        return max - min;
     }
 }
